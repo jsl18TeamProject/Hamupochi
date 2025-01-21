85f49a607da99af78fa080365e12b1d921364080
Minami <151480603+Minami127@users.noreply.github.com>
2025-01-21 16:50:17 +0900

    Delete HamuPochi/src/main/java/com/example/HamuPochi/Util/Config.java

--- HamuPochi/src/main/java/com/example/HamuPochi/Util/Config.java
+++
@@ -1,8 +0,0 @@
-package com.example.HamuPochi.Util;
-
-public class Config {
-
-    public static final String AWS_KEY = "AKIAZPPGAJTXIKJE4NVA";
-    public static final String AWS_SECRET_KEY = "GHfosTbTw9CI7wlc6di5w4BnQyXvnGW2MkRHuvI/";
-
-}
